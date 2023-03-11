e0edc968dcedd40ded45ceaf3ef08aea01c7ee5d
phamdinhlam105 <heartsstealer105@gmail.com>
2023-03-11 10:47:45 +0700

    synchronize

--- vong_lap/Bai_2.py
+++ vong_lap/Bai_2.py
@@ -1,5 +1,5 @@
 n = int(input("Nhap vao vi tru phan tu trong day so 50, 51, 53, 56, 60: "))
-s=50
+temp=50
 for i in range(0,n):
-   s += (i+1)
+   temp += (i+1)
 print("phan tu thu %d la : %d" %(n, s))
